c6c3f791877b595d3bdf7793888283263b9a2de1
gemilepus <31269075+gemilepus@users.noreply.github.com>
2022-10-04 13:02:08 +0800

    Update main.py

--- ch05-shallow_net_in_keras/main.py
+++ ch05-shallow_net_in_keras/main.py
@@ -16,10 +16,10 @@ print(y_train.shape)
 print("y_train[0:12]:")
 print(y_train[0:12])
 
-plt.figure(figsize=(5,5))
+plt.figure(figsize=(5, 5))
 for k in range(12):
-  plt.subplot(3, 4, k+1)
-  plt.imshow(X_train[k], cmap='gray')
+    plt.subplot(3, 4, k + 1)
+    plt.imshow(X_train[k], cmap='gray')
 plt.tight_layout()
 plt.show()
 
@@ -37,14 +37,14 @@ y_train = tf.keras.utils.to_categorical(y_train, n_classes)
 y_test = tf.keras.utils.to_categorical(y_test, n_classes)
 
 model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Dense(64, activation='sigmoid', input_shape=(784, )))
+model.add(tf.keras.layers.Dense(64, activation='sigmoid', input_shape=(784,)))
 model.add(tf.keras.layers.Dense(10, activation='softmax'))
 
 model.compile(loss='mean_squared_error',
               optimizer=tf.keras.optimizers.SGD(learning_rate=0.01),
               metrics=['accuracy'])
 
-model.fit(X_train,y_train,
+model.fit(X_train, y_train,
           batch_size=128,
           epochs=200,
           verbose=1,
